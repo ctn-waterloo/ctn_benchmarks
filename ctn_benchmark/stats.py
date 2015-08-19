1ec539e1809afd2a7a64b7013a80c55bc83a8ab4
Terry Stewart <tcstewar@uwaterloo.ca>
2015-08-19 15:27:40 -0400

    Added bootstrap code

--- ctn_benchmark/stats.py
+++ ctn_benchmark/stats.py
@@ -6,3 +6,11 @@ def find_offset(a, b):
     index = np.argmax(corr[len(a):])
     return index
 
+def bootstrapci(data, func, n=3000, p=0.95):
+    index=int(n*(1-p)/2)
+
+    samples = np.random.choice(data, size=n)
+    r = [func(s) for s in samples]
+    r.sort()
+    return r[index], r[-index]
+
