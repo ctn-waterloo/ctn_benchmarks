0de504158a00e7e7ffbc3d9b897b32b248bcf658
Terry Stewart <tcstewar@uwaterloo.ca>
2015-10-06 13:34:34 +0100

    Added spa splitting of passthrough nodes

--- ctn_benchmark/spa/memory.py
+++ ctn_benchmark/spa/memory.py
@@ -11,10 +11,11 @@ import nengo.spa as spa
 
 import ctn_benchmark
 
+import split
+
 class SemanticMemory(ctn_benchmark.Benchmark):
     def params(self):
         self.default('time per symbol', time_per_symbol=0.2)
-        self.default('time per cue', time_per_cue=0.1)
         self.default('number of symbols', n_symbols=4)
         self.default('time to recall', T=1.0)
         self.default('dimensions', D=16)
@@ -25,12 +26,9 @@ class SemanticMemory(ctn_benchmark.Benchmark):
             model.word = spa.State(dimensions=p.D)
             model.marker = spa.State(dimensions=p.D)
             model.memory = spa.State(dimensions=p.D, feedback=1)
-            model.motor = spa.State(dimensions=p.D)
-            model.cue = spa.State(dimensions=p.D)
 
             model.cortical = spa.Cortical(spa.Actions(
                 'memory = word * marker',
-                'motor = memory * ~cue',
                 ))
 
             def word(t):
@@ -45,21 +43,12 @@ class SemanticMemory(ctn_benchmark.Benchmark):
                     return 'M%d' % index
                 return '0'
 
-            def cue(t):
-                index = (t - p.time_per_symbol * p.n_symbols) / p.time_per_cue
-                if index > 0:
-                    index = index % (2 * p.n_symbols)
-                    if index < p.n_symbols:
-                        return 'S%d' % index
-                    else:
-                        return 'M%d' % (index - p.n_symbols)
-                return '0'
-
-            model.input = spa.Input(word=word, marker=marker, cue=cue)
+            model.input = spa.Input(word=word, marker=marker)
 
             self.p_memory = nengo.Probe(model.memory.output, synapse=0.03)
-            self.p_motor = nengo.Probe(model.motor.output, synapse=0.03)
-            self.vocab = model.get_output_vocab('motor')
+            self.vocab = model.get_output_vocab('memory')
+
+        self.replaced = split.split_passthrough(model, max_dim=16)
         return model
     def evaluate(self, p, sim, plt):
         T = p.T + p.time_per_symbol * p.n_symbols
@@ -69,42 +58,23 @@ class SemanticMemory(ctn_benchmark.Benchmark):
         pairs = np.zeros((p.n_symbols, p.D), dtype=float)
         for i in range(p.n_symbols):
             pairs[i] = self.vocab.parse('S%d*M%d' % (i, i)).v
-        memory = np.dot(pairs, sim.data[self.p_memory].T).T
-        motor = self.vocab.dot(sim.data[self.p_motor].T).T
+
+        if self.p_memory not in self.replaced:
+            data_memory = sim.data[self.p_memory]
+        else:
+            data_memory = np.hstack([sim.data[pr]
+                                     for pr in self.replaced[self.p_memory]])
+        memory = np.dot(pairs, data_memory.T).T
 
         times = sim.trange()
 
         mean_memory = np.mean(memory[-1])
 
-        mag_correct = []
-        mag_others = []
-        mag_second = []
-        index = 0
-        for i in range(int(p.T / p.time_per_cue)):
-            t = p.time_per_symbol * p.n_symbols + (i+1) * p.time_per_cue
-            while index < len(times) - 1 and times[index+1] < t:
-                index += 1
-            correct = i % (2 * p.n_symbols)
-            if correct < p.n_symbols:
-                correct_index = self.vocab.keys.index('M%d' % correct)
-            else:
-                correct_index = self.vocab.keys.index('S%d' %
-                                                      (correct - p.n_symbols))
-            mag_correct.append(motor[index, correct_index])
-            mag_others.append(np.mean(np.delete(motor[index], [correct_index])))
-            mag_second.append(np.max(np.delete(motor[index], [correct_index])))
-
         if plt is not None:
-            plt.subplot(2, 1, 1)
             plt.plot(sim.trange(), memory)
-            plt.subplot(2, 1, 2)
-            plt.plot(sim.trange(), motor)
 
 
-        return dict(mag_correct=np.mean(mag_correct),
-                    mag_others=np.mean(mag_others),
-                    mag_second=np.mean(mag_second),
-                    memory = mean_memory)
+        return dict(memory = mean_memory)
 
 if __name__ == '__main__':
     SemanticMemory().run()
--- ctn_benchmark/spa/split.py
+++ ctn_benchmark/spa/split.py
@@ -1,13 +1,6 @@
-"""
-Nengo Benchmark Model: SPA Sequence
+import numpy as np
+import nengo
 
-Given no input, the model will cycle between cortical states using a
-basal ganglia and thalamus.
-"""
-
-import split
-
-'''
 def gather_info(network, inputs, outputs, parents):
     for c in network.connections:
         if c.post_obj not in inputs:
@@ -28,6 +21,7 @@ def gather_info(network, inputs, outputs, parents):
         gather_info(net, inputs, outputs, parents)
 
 def split_passthrough(model, max_dim=16):
+    replaced = {}
     inputs = {}
     outputs = {}
     parents = {}
@@ -58,7 +52,7 @@ def split_passthrough(model, max_dim=16):
                     nodes.append(new_node)
                     index += size
 
-                for c in inputs[node][:]:
+                for c in inputs.get(node, [])[:]:
                     base_transform = c.transform
                     if len(base_transform.shape) == 0:
                         base_transform = np.eye(c.size_mid) * base_transform
@@ -71,6 +65,7 @@ def split_passthrough(model, max_dim=16):
                             with parents[c]:
                                 new_c = nengo.Connection(c.pre, n,
                                                  transform=t,
+                                                 function=c.function,
                                                  synapse=c.synapse)
                                 inputs[n].append(new_c)
                                 outputs[c.pre_obj].append(new_c)
@@ -79,7 +74,7 @@ def split_passthrough(model, max_dim=16):
                     inputs[node].remove(c)
                     parents[c].connections.remove(c)
 
-                for c in outputs[node][:]:
+                for c in outputs.get(node, [])[:]:
                     base_transform = c.transform
                     if len(base_transform.shape) == 0:
                         base_transform = np.eye(c.size_mid) * base_transform
@@ -101,78 +96,16 @@ def split_passthrough(model, max_dim=16):
                     parents[c].connections.remove(c)
 
                 parents[node].nodes.remove(node)
+                replaced[node] = nodes
 
-'''
+    for p in model.probes[:]:
+        if p.target in replaced:
+            model.probes.remove(p)
+            probes = []
+            for n in replaced[p.target]:
+                with model:
+                    probe = nengo.Probe(n, synapse=p.synapse)
+                probes.append(probe)
+            replaced[p] = probes
 
-
-
-
-
-import ctn_benchmark
-import numpy as np
-import nengo
-import nengo.spa as spa
-
-class SPASequenceRouted(ctn_benchmark.Benchmark):
-    def params(self):
-        self.default('dimensionality', D=32)
-        self.default('number of actions', n_actions=5)
-        self.default('time to simulate', T=1.0)
-        self.default('starting action', start=0)
-
-    def model(self, p):
-        model = spa.SPA()
-        with model:
-            model.vision = spa.Buffer(dimensions=p.D)
-            model.state = spa.Memory(dimensions=p.D)
-            actions = ['dot(state, S%d) --> state=S%d' % (i,(i+1))
-                       for i in range(p.n_actions - 1)]
-            actions.append('dot(state, S%d) --> state=vision' %
-                           (p.n_actions - 1))
-            model.bg = spa.BasalGanglia(actions=spa.Actions(*actions))
-            model.thal = spa.Thalamus(model.bg)
-
-            model.input = spa.Input(vision='S%d' % p.start)
-
-            self.probe = nengo.Probe(model.thal.actions.output, synapse=0.03)
-        split.split_passthrough(model)
-
-        return model
-
-    def evaluate(self, p, sim, plt):
-        sim.run(p.T)
-        self.record_speed(p.T)
-
-        index = int(0.05 / p.dt)  # ignore the first 50ms
-        best = np.argmax(sim.data[self.probe][index:], axis=1)
-        times = sim.trange()
-        change = np.diff(best)
-        change_points = np.where(change != 0)[0]
-        intervals = np.diff(change_points * p.dt)
-
-        best_index = best[change_points][1:]
-        route_intervals = intervals[np.where(best_index == p.n_actions-1)[0]]
-        seq_intervals = intervals[np.where(best_index != p.n_actions-1)[0]]
-
-        data = sim.data[self.probe][index:]
-        peaks = [np.max(data[change_points[i]:change_points[i+1]])
-                 for i in range(len(change_points)-1)]
-
-        if plt is not None:
-            plt.plot(times, sim.data[self.probe])
-            plt.plot(times[index + 1:], np.where(change!=0,1,0))
-
-            for i, peak in enumerate(peaks):
-                plt.hlines(peak, times[change_points[i] + index],
-                                  times[change_points[i+1] + index])
-
-
-
-        return dict(period=np.mean(seq_intervals),
-                    period_sd=np.std(seq_intervals),
-                    route_period=np.mean(route_intervals),
-                    route_period_sp=np.std(route_intervals),
-                    peak=np.mean(peaks), peak_sd=np.std(peaks))
-
-if __name__ == '__main__':
-    SPASequenceRouted().run()
+    return replaced
