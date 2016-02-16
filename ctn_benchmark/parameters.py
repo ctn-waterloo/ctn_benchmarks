074bf980c6ac146e82c9c481397433e105ab0fc9
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-02-16 18:59:02 -0500

    Add basic parameters implementation.

---
+++ ctn_benchmark/parameters.py
@@ -0,0 +1,69 @@
+from collections import MutableMapping
+
+
+class Parameter(object):
+    def __init__(self, name, description, default, param_type=None):
+        if param_type is None:
+            param_type = type(default)
+
+        self.name = name
+        self.description = description
+        self.default = default
+        self.param_type = param_type
+        self._value = default
+        self._is_set = False
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._is_set = True
+        self._value = value
+
+    @property
+    def is_set(self):
+        return self._is_set
+
+    def reset(self):
+        self._is_set = False
+        self._value = self.default
+
+
+class ParameterSet(MutableMapping):
+    def __init__(self):
+        super(ParameterSet, self).__setattr__('params', {})
+
+    def add_parameter(self, param):
+        if param.name in self.params:
+            raise ValueError(
+                "Parameter {0} already exists.".format(param.name))
+        self.params[param.name] = param
+
+    def add_default(self, description, param_type=None, **kwargs):
+        if len(kwargs) != 1:
+            raise ValueError("Must specifiy exactly one parameter.")
+        k, v = kwargs.items()[0]
+        self.add_parameter(Parameter(k, description, v, param_type=param_type))
+
+    def __getitem__(self, key):
+        return self.params[key].value
+
+    def __setitem__(self, key, value):
+        self.params[key].value = value
+
+    def __delitem__(self, key):
+        self.params[key].reset()
+
+    def __len__(self):
+        return len(self.params)
+
+    def __iter__(self):
+        return iter(self.params)
+
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
---
+++ ctn_benchmark/tests/test_parameters.py
@@ -0,0 +1,79 @@
+import pytest
+
+from ctn_benchmark import parameters
+
+
+class TestParameter(object):
+    @pytest.mark.parametrize('t,v', [
+        (int, 42),
+        (str, 'lorem ipsum'),
+        (float, 0.1),
+        (parameters.ParameterSet, parameters.ParameterSet())])
+    def test_type_inference(self, t, v):
+        p = parameters.Parameter('p', "desc", default=v)
+        assert p.param_type is t
+
+    def test_initializes_value_with_default(self):
+        p = parameters.Parameter('p', "desc", default=23)
+        assert p.value == 23
+
+    def test_knows_whether_it_was_set(self):
+        p = parameters.Parameter('p', "desc", default=0)
+        assert p.is_set is False
+        p.value = 0
+        assert p.is_set is True
+        p.reset()
+        assert p.is_set is False
+
+    def test_reset_restores_default(self):
+        p = parameters.Parameter('p', "desc", default=0)
+        assert p.value == 0
+        p.value = 1
+        p.reset()
+        assert p.value == 0
+
+
+class TestParameterSet(object):
+    @pytest.fixture()
+    def ps(self):
+        return parameters.ParameterSet()
+
+    def test_can_add_parameter(self, ps):
+        p = parameters.Parameter('p', "desc", default=0)
+        ps.add_parameter(p)
+        assert 'p' in ps.params
+        assert ps.params['p'] is p
+
+    def test_does_not_allow_duplicates(self, ps):
+        with pytest.raises(ValueError):
+            ps.add_parameter(parameters.Parameter('p', "desc", default=0))
+            ps.add_parameter(parameters.Parameter('p', "desc", default=0))
+
+    def test_supports_add_default_shorthand(self, ps):
+        ps.add_default("desc", p=2)
+        assert 'p' in ps.params
+        assert ps.params['p'].default == 2
+
+    def test_default_shorthand_does_not_allow_adding_two_params_at_same_time(
+            self, ps):
+        with pytest.raises(ValueError):
+            ps.add_default("desc", p1=1, p2=2)
+
+    def test_provides_dict_like_access_to_parameter_values(self, ps):
+        ps.add_parameter(parameters.Parameter('p', "desc", default=0))
+        assert ps['p'] == 0
+        ps['p'] = 1
+        assert ps['p'] == 1
+        del ps['p']  # should do a reset
+        assert ps['p'] == 0
+
+        assert len(ps) == 1
+        assert list(iter(ps)) == ['p']
+
+    def test_provides_attribute_like_access_to_parameter_values(self, ps):
+        ps.add_parameter(parameters.Parameter('p', "desc", default=0))
+        assert ps.p == 0
+        ps.p = 1
+        assert ps.p == 1
+        ps.params['p'].reset()
+        assert ps.p == 0
